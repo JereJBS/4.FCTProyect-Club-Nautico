0e6912c69b123374a634ec62c1c5d63f1fb2cfa5
JereJBS <jerestudiios@gmail.com>
2024-04-08 10:09:35 +0100

    first commit

---
+++ src/main/java/com/nauticclub4/FCTProyectJPA/Application.java
@@ -0,0 +1,13 @@
+package com.nauticclub4.FCTProyectJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nauticclub4/FCTProyectJPA/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nauticclub4.FCTProyectJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
