0207bba0285922bdb35175f7880e46d3ae0e064c
JereJBS <jerestudiios@gmail.com>
2024-04-09 11:34:42 +0100

    SocioRepository done

---
+++ src/main/java/com/nauticclub4/FCTProyectJPA/repository/Socio.java
@@ -0,0 +1,7 @@
+package com.nauticclub4.FCTProyectJPA.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Socio extends JpaRepository<Socio, Long>{
+    
+}
