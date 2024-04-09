839e12df1d5ab419f385b2bbbae35d0990bd72fb
JereJBS <jerestudiios@gmail.com>
2024-04-09 11:33:10 +0100

    SalidasRepository done

---
+++ src/main/java/com/nauticclub4/FCTProyectJPA/repository/SalidasRepository.java
@@ -0,0 +1,9 @@
+package com.nauticclub4.FCTProyectJPA.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nauticclub4.FCTProyectJPA.entities.Salidas;
+
+public interface SalidasRepository extends JpaRepository<Salidas, Long>{
+    
+}
