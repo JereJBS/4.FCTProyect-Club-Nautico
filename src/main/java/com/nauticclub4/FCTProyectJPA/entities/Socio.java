040621e52c74940d9640732da5eac23aec0ad5e6
JereJBS <jerestudiios@gmail.com>
2024-04-09 12:53:45 +0100

    Fixed Socio

--- src/main/java/com/nauticclub4/FCTProyectJPA/entities/Socio.java
+++ src/main/java/com/nauticclub4/FCTProyectJPA/entities/Socio.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,8 +41,11 @@ public class Socio {
 	@Column(name = "email", nullable = false)
 	private String email;
 
-	@JsonIgnore
-	@OneToMany(mappedBy="socio", cascade=CascadeType.ALL, orphanRemoval=true)
-	private List<Barco> barcos;
+	// @JsonIgnore
+	// @OneToMany(mappedBy="socio", cascade=CascadeType.ALL, orphanRemoval=true)
+	// private List<Barco> barcos;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Barco> barcos;
 
 }
