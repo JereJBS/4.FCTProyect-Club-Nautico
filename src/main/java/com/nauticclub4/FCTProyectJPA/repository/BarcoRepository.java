878a57af68b397536c0d6d762b42f6bee1598326
JereJBS <jerestudiios@gmail.com>
2024-04-09 11:30:01 +0100

    BarcoRepository done

---
+++ src/main/java/com/nauticclub4/FCTProyectJPA/repository/BarcoRepository.java
@@ -0,0 +1,9 @@
+package com.nauticclub4.FCTProyectJPA.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nauticclub4.FCTProyectJPA.entities.Barco;
+
+public interface BarcoRepository extends JpaRepository<Barco, Long>{
+    
+}
