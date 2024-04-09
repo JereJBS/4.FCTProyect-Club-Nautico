ebcbae40526046cd4d0ea034ef1f97bed7ad27d8
JereJBS <jerestudiios@gmail.com>
2024-04-09 11:26:43 +0100

    Socio entity done

--- src/main/java/com/nauticclub4/FCTProyectJPA/entities/Socio.java
+++ src/main/java/com/nauticclub4/FCTProyectJPA/entities/Socio.java
@@ -1,5 +1,6 @@
 package com.nauticclub4.FCTProyectJPA.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,9 +8,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @Data
 @RequiredArgsConstructor
+@ToString
 @Entity
 @Table(name = "socio")
 public class Socio {
@@ -18,7 +21,17 @@ public class Socio {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+
+	@Column(name = "nombre", nullable = false)
 	private String name;
+
+	@Column(name = "direccion", nullable = false)
 	private String direccion;
+
+	@Column(name = "telefono", nullable = false)
+	private String telefono;
+
+	@Column(name = "email", nullable = false)
+	private String email;
 
 }
