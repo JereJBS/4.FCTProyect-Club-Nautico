ba957be1eeb33085b4ecd60b7c7ca558d7ba249e
JereJBS <jerestudiios@gmail.com>
2024-04-09 10:59:57 +0100

    patron entity done

--- src/main/java/com/nauticclub4/FCTProyectJPA/entities/patron.java
+++ src/main/java/com/nauticclub4/FCTProyectJPA/entities/patron.java
@@ -1,5 +1,6 @@
 package com.nauticclub4.FCTProyectJPA.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,9 +8,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @Data
 @RequiredArgsConstructor
+@ToString
 @Entity
 @Table(name = "patron")
 public class Patron {
@@ -17,7 +20,10 @@ public class Patron {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(name = "name", nullable = false)
 	private String name;
+
+	@Column(name = "dni", nullable = false)
 	private String dni;
 }
